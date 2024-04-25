99b3f9bdf6cb103cde0be5f49b4d25d44a41db22
ben-oneil-icustomer <138517049+ben-oneil-icustomer@users.noreply.github.com>
2024-04-25 12:43:57 -0400

    adding everything

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBgpxpyFWp4V2DDlb5UYyfUL0q9dP_e_xI",
+    authDomain: "music-player-b09dc.firebaseapp.com",
+    projectId: "music-player-b09dc",
+    storageBucket: "music-player-b09dc.appspot.com",
+    messagingSenderId: "914650393613",
+    appId: "1:914650393613:web:3264d8548d33cf06ea3944",
+    measurementId: "G-P4T88WLHQF"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBgpxpyFWp4V2DDlb5UYyfUL0q9dP_e_xI",
+    authDomain: "music-player-b09dc.firebaseapp.com",
+    projectId: "music-player-b09dc",
+    storageBucket: "music-player-b09dc.appspot.com",
+    messagingSenderId: "914650393613",
+    appId: "1:914650393613:web:3264d8548d33cf06ea3944",
+    measurementId: "G-P4T88WLHQF"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
